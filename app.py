0a9912737b8f0b4e895c8ccdb532ed6bd5e8f802
Petr Parachin <petr.parachin@gmail.com>
2023-04-13 03:11:01 +0200

    Initial files

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_migrate import Migrate
+
+import genders
+import genres
+import nationalities
+import playlists
+import users
+from db import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://user:toor@localhost:3306/ki_gui_db'
+# Pomoci pymysql python driveru v pripade, ze je problem s mysql driverem
+#app.config['SQLALCHEMY_DATABASE_URI'] = \
+#     'mysql+pymysql://user:toor@localhost:3306/ki_gui_db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_mapping(SECRET_KEY="dev")
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+app.register_blueprint(genres.bp)
+app.register_blueprint(nationalities.bp)
+app.register_blueprint(users.bp)
+app.register_blueprint(genders.bp)
+app.register_blueprint(playlists.bp)
+
+
+@app.route('/')
+def index_view():
+    return render_template('index.html', title="SQLAlchemyORM - Home")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ users/models.py
@@ -0,0 +1,15 @@
+from db import db
+from genders.models import Genders
+from sqlalchemy.orm import relationship
+
+
+class Users(db.Model):
+    id_u = db.Column(db.Integer, primary_key=True)
+    profile_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    date_of_birth = db.Column(db.DateTime, nullable=False)
+    gender = db.Column(db.Integer, db.ForeignKey(Genders.id_g), nullable=False)
+    gender_f = db.relationship("Genders", lazy="select", backref=db.backref("Genders", lazy="joined"))
+    created_at = db.Column(db.DateTime, nullable=True)
+    updated_at = db.Column(db.DateTime, nullable=True)
